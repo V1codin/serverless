8b3f6ac19510e743357e99172a8be796cfe3191c
kir <blindmonk46@gmail.com>
2023-11-07 15:31:07 +0200

    task 11 v1.00 feat: add app host to env

--- 11_jsonbase/src/constants.ts
+++ 11_jsonbase/src/constants.ts
@@ -1,4 +1,4 @@
 export const SERVER_ERROR_MESSAGE = 'Internal server error';
 export const APP_PORT = Number(process.env['LOCAL_PORT']) || 3000;
-export const APP_HOST = 'http://localhost';
+export const APP_HOST = process.env['LOCAL_HOST'] || 'http://localhost';
 export const DB_FILENAME = 'db.json';
